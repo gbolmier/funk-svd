a601b148513afc289197ee4776bf2a089cd35659
Joao Felipe Guedes <joao.guedes@mjv.com.br>
2020-04-19 13:56:56 -0300

    Including method SVD.get_utility_matrix

--- run_experiment.py
+++ run_experiment.py
@@ -6,7 +6,6 @@ from funk_svd import SVD
 
 from sklearn.metrics import mean_absolute_error
 
-
 df = fetch_ml_ratings(variant='100k')
 
 train = df.sample(frac=0.8, random_state=7)
@@ -16,9 +15,19 @@ test = df.drop(train.index.tolist()).drop(val.index.tolist())
 svd = SVD(learning_rate=0.001, regularization=0.005, n_epochs=100,
           n_factors=15, min_rating=1, max_rating=5)
 
+df_matrix_original = svd.get_utility_matrix(df)
+print ("Original Utility Matrix: \n", df_matrix_original.values)
+
+# Getting all u_id and i_id combinations
+df_user_item = pd.melt(df_matrix_original.reset_index(drop=False), id_vars='u_id')
+
 svd.fit(X=train, X_val=val, early_stopping=True, shuffle=False)
 
-pred = svd.predict(test)
-mae = mean_absolute_error(test["rating"], pred)
+pred_test = svd.predict(test)
+df_user_item["rating"] = svd.predict(df_user_item)
+
+print ("Predicted Utility Matrix: \n", svd.get_utility_matrix(df_user_item).values)
+
+mae = mean_absolute_error(test["rating"], pred_test)
 
 print(f'Test MAE: {mae:.2f}')
